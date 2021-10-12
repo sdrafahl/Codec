7fb06ffa8483cb6ee0f56f9bdecd10c783f594d0
sdrafahl <shanedrafahl@gmail.com>
2021-10-11 21:42:18 -0700

    Writing Codec generator using Circe

--- CodecGeneric/src/main/scala/ErrorModels/ParsingError.scala
+++ CodecGeneric/src/main/scala/ErrorModels/ParsingError.scala
@@ -4,11 +4,10 @@ import cats.Show
 import cats.implicits._
 
 abstract class ParsingError
+sealed case class StringParsingErrorWithInput[A](message: String, input: A) extends ParsingError
+sealed case class StringParsingError(message: String) extends ParsingError
 
-object ParsingError {
-  sealed case class StringParsingErrorWithInput[A](message: String, input: A) extends ParsingError
-  sealed case class StringParsingError(message: String) extends ParsingError
-
+object ParsingError {  
   given ParsingErrorShow: Show[ParsingError] = Show.fromToString[ParsingError] 
 }
 
